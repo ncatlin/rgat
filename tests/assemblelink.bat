928e05791887351636f07700b35a5f96dac4ff77
Nia Catlin <nia.catlin.2012@live.rhul.ac.uk>
2017-09-03 13:43:01 +0100

    cd to test dir in win assemble script

--- tests/assemblelink.bat
+++ tests/assemblelink.bat
@@ -1,10 +1,13 @@
 @echo off
 
+set TESTSPATH="C:\Users\nia\Source\Repos\rgat\release\output\tests"
 set NASMPATH="C:\Users\nia\AppData\Local\bin\NASM\nasm.exe"
 set LINKERPATH="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.10.25017\bin\HostX64\x64\link.exe"
 set LIBS32="C:\Program Files (x86)\Windows Kits\10\Lib\10.0.15063.0\um\x86"
 set LIBS64="C:\Program Files (x86)\Windows Kits\10\Lib\10.0.15063.0\um\x64"
 
+;---------------------------
+cd TESTSPATH
 for %%f in (*.86.asm) do (
             echo "assembling %%~nf.asm"
             call :assemble32 "%%~nf"
@@ -16,16 +19,18 @@ for %%f in (*.64.asm) do (
     )
 	
 goto end
-	
+;-----------------------------
 
 :assemble32
 %NASMPATH% -fwin32 %1.asm
 %LINKERPATH% %1.obj /subsystem:console /entry:main  /libpath:%LIBS32% /nodefaultlib kernel32.lib user32.lib /largeaddressaware:no
 goto:EOF
 
+;-----------------------------
 :assemble64
 %NASMPATH% -fwin64 %1.asm
 %LINKERPATH% %1.obj /subsystem:console /entry:main  /libpath:%LIBS64% /nodefaultlib kernel32.lib user32.lib
 goto:EOF
 
+;-----------------------------
 :end
