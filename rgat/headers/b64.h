21a9acc9761c4ca10291c546c79a6a6d4811131c
Nia Catlin <niacatn@gmail.com>
2021-02-16 01:14:35 +0000

    remove old C++ code from merge

--- rgat/headers/b64.h
+++
@@ -1,6 +0,0 @@
-//http://www.adp-gmbh.ch/cpp/common/base64.html
-#pragma once
-#include <string>
-
-std::string base64_encode(unsigned char const*, unsigned int len);
-std::string base64_decode(std::string const& s);
